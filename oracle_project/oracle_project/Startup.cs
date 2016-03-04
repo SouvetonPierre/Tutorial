3bf13c17504bc8ede59f96d8e94470a9a2551982
SouvetonPierre <pierresouveton@gmail.com>
2016-03-04 17:01:28 +0100

    Oracle
    Projet Oracle pour EPSI

---
+++ oracle_project/oracle_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(oracle_project.Startup))]
+namespace oracle_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
